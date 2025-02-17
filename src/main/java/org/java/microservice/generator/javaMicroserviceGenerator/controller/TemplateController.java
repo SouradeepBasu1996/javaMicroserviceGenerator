3153526ca2d145397a38df0deafcf139cc67137d
Souradeep Basu <souradeep@Souradeeps-MacBook-Air.local>
2025-02-17 11:59:40 +0530

    Enabling CORS

--- src/main/java/org/java/microservice/generator/javaMicroserviceGenerator/controller/TemplateController.java
+++ src/main/java/org/java/microservice/generator/javaMicroserviceGenerator/controller/TemplateController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.nio.file.Path;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class TemplateController {
